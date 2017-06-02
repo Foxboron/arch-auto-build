73f15f459a08d1c8a911b0892f2a7b8d7a5e7cb7
Morten Linderud <morten@linderud.pw>
2017-06-02 14:50:18 +0200

    Fixed docker
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- docker/add-repo.sh
+++ docker/add-repo.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-if [ ! -d "/var/cache/pacman/$1" ]; then
+if [ ! -d "/repos/$1/$1.db.tar" ]; then
 	echo "Include = /etc/pacman.d/$1" >> /etc/pacman.conf
-	mkdir /repos/$1
-	chmod 777 /repos/$1
-	chown -R nobody:nobody /var/cache/pacman/$1
 	cat >/etc/pacman.d/$1<<EOL
 [options]
 CacheDir = /var/cache/pacman/pkg
@@ -14,7 +11,8 @@ CleanMethod = KeepCurrent
 [$1]
 SigLevel = Optional TrustAll
 Server = file:///repos/$1
-	EOL
+EOL
 	repo-add /repos/$1/$1.db.tar
+	chown -R nobody:nobody /repos/$1
 
 fi
--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ -z ${var+x} ]; then 
+if [ -z "$1" ]; then 
 	echo "We need to initialize a repositry. ./build-docker.sh {{repo}}";
 	exit
 fi
 docker build -t arch-build .
-ID=$(docker run --privileged -it -v /srv/repo/$1:/var/cache/pacman/$1 -v /sys/fs/cgroup/systemd/docker:/sys/fs/cgroup/systemd/docker -d arch-build bash)
+ID=$(docker run --privileged -it -v /srv/repo/$1:/repos/$1 -v /sys/fs/cgroup/systemd/docker:/sys/fs/cgroup/systemd/docker -d arch-build bash)
 echo $ID
 docker exec -it $ID /usr/bin/install-chroot.sh
 docker exec -it $ID /usr/bin/add-repo.sh $1
