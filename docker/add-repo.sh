64aa7378622e92cec23929759d83f7dc2e4bcae3
Morten Linderud <morten@linderud.pw>
2017-06-02 14:26:41 +0200

    Patches
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- docker/add-repo.sh
+++ docker/add-repo.sh
@@ -2,19 +2,19 @@
 
 if [ ! -d "/var/cache/pacman/$1" ]; then
 	echo "Include = /etc/pacman.d/$1" >> /etc/pacman.conf
-	mkdir /var/cache/pacman/$1
-	chmod 777 /var/cache/pacman/$1
+	mkdir /repos/$1
+	chmod 777 /repos/$1
 	chown -R nobody:nobody /var/cache/pacman/$1
 	cat >/etc/pacman.d/$1<<EOL
 [options]
 CacheDir = /var/cache/pacman/pkg
-CacheDir = /var/cache/pacman/$1
+CacheDir = /repos/$1
 CleanMethod = KeepCurrent
 
 [$1]
 SigLevel = Optional TrustAll
-Server = file:///var/cache/pacman/$1
+Server = file:///repos/$1
 	EOL
-	repo-add /var/cache/pacman/$1/$1.db.tar
+	repo-add /repos/$1/$1.db.tar
 
 fi
--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cower -p */PKGBUILD -dd
+chown -R nobody:nobody *
 aurqueue * > queue
-sudo -u nobody aurbuild -c -a queue
+sudo -u nobody aurbuild -c -d $1 -a queue
 rm -rf /var/lib/aurbuild/x86_64/nobody*
+exit
