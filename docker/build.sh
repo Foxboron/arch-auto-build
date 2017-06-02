812b158f7cf95708ea1203e4216fe33a96eb3d37
Morten Linderud <morten@linderud.pw>
2017-06-02 14:59:39 +0200

    Comments
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Check if there is a repo for this
+add-repo.sh $1
+
+# lets get the AUR PKGBUILDS
 cower -p */PKGBUILD -dd
 chown -R nobody:nobody *
 aurqueue * > queue
+
+# aurutils will do the building and repo management
 sudo -u nobody aurbuild -c -d $1 -a queue
+
+# Cleanup
 rm -rf /var/lib/aurbuild/x86_64/nobody*
 exit
