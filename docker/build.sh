799a76021e4e06a50bb955ddb581d2a778a845df
Morten Linderud <morten@linderud.pw>
2017-06-06 15:02:55 +0200

    Wrong bash args
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- docker/build.sh
+++ docker/build.sh
@@ -15,11 +15,11 @@ chown -R nobody:nobody *
 # Check all packages for something inn our repo.
 # If it is there, we dont care for the AUR package
 check(){
-    if [[ $(pacman -Sl $REPO | grep $1 | cut -d" " -f2) == "" || $PKG == $1 ]]; then
-        echo $pkg
+    if [[ $(pacman -Sl "$REPO" | grep "$1" | cut -d" " -f2) == "" || $PKG == $1 ]]; then
+        echo $1
     fi
 }
-aurchain * | while read -r pkg _; do check $PKG; done > /tmp/queue
+aurchain * | while read -r CHAINPKG _; do check $CHAINPKG; done
 
 # aurutils will do the building and repo management
 sudo -u nobody aurbuild -c -d $REPO -a /tmp/queue
